368f82884036d437558a5ca3517a5f29ba37a363
Puneet Singh <puneet.singh@limetray.com>
2018-06-03 18:46:29 +0530

    added form submit js

--- js/application.js
+++ js/application.js
@@ -1,99 +1,30 @@
-jQuery(document).ready(function( $ ) {
+$('.submit-form').click((e) => {
+  e.preventDefault();
+  let  name = $('input[name="name"]').val();
+  let  email = $('input[name="email"]').val();
+  let  phone = $('input[name="phone"]').val();
+  let  comments = $('textarea[name="comments"]').val();
 
-  // Back to top button
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn('slow');
-    } else {
-      $('.back-to-top').fadeOut('slow');
-    }
-  });
-  $('.back-to-top').click(function(){
-    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
-    return false;
-  });
 
-  // Initiate superfish on nav menu
-//   $('.nav-menu').superfish({
-//     animation: {
-//       opacity: 'show'
-//     },
-//     speed: 400
-//   });
-
-  // Mobile Navigation
-  if ($('#nav-menu-container').length) {
-    var $mobile_nav = $('#nav-menu-container').clone().prop({
-      id: 'mobile-nav'
-    });
-    $mobile_nav.find('> ul').attr({
-      'class': '',
-      'id': ''
-    });
-    $('body').append($mobile_nav);
-    $('body').prepend('<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>');
-    $('body').append('<div id="mobile-body-overly"></div>');
-    $('#mobile-nav').find('.menu-has-children').prepend('<i class="fa fa-chevron-down"></i>');
-
-    $(document).on('click', '.menu-has-children i', function(e) {
-      $(this).next().toggleClass('menu-item-active');
-      $(this).nextAll('ul').eq(0).slideToggle();
-      $(this).toggleClass("fa-chevron-up fa-chevron-down");
-    });
-
-    $(document).on('click', '#mobile-nav-toggle', function(e) {
-      $('body').toggleClass('mobile-nav-active');
-      $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-      $('#mobile-body-overly').toggle();
-    });
-
-    $(document).click(function(e) {
-      var container = $("#mobile-nav, #mobile-nav-toggle");
-      if (!container.is(e.target) && container.has(e.target).length === 0) {
-        if ($('body').hasClass('mobile-nav-active')) {
-          $('body').removeClass('mobile-nav-active');
-          $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-          $('#mobile-body-overly').fadeOut();
-        }
-      }
-    });
-  } else if ($("#mobile-nav, #mobile-nav-toggle").length) {
-    $("#mobile-nav, #mobile-nav-toggle").hide();
-  }
-
-  // Smooth scroll for the menu and links with .scrollto classes
-  $('.nav-menu a, #mobile-nav a, .scrollto').on('click', function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      if (target.length) {
-        var top_space = 0;
-
-        if ($('#header').length) {
-          top_space = $('#header').outerHeight();
-
-          if( ! $('#header').hasClass('header-fixed') ) {
-            top_space = top_space - 20;
+  if(name != '' && email != '' && phone != '' && comments != ''){
+    $('.contact-error').addClass('hide');
+    $('.loader').removeClass('hide');
+    $('.contactus-form').hide();
+    $.post("../common/form/create.php",
+      {
+          name, email, phone, comments
+      },
+      function(data, status){
+          if(status=='success'){
+            $('.loader').addClass('hide');
+            $('.contact-success').removeClass('hide').html('Thank you for reaching out to us. We will get in touch with you shortly.');
           }
-        }
-
-        $('html, body').animate({
-          scrollTop: target.offset().top - top_space
-        }, 1500, 'easeInOutExpo');
-
-        if ($(this).parents('.nav-menu').length) {
-          $('.nav-menu .menu-active').removeClass('menu-active');
-          $(this).closest('li').addClass('menu-active');
-        }
-
-        if ($('body').hasClass('mobile-nav-active')) {
-          $('body').removeClass('mobile-nav-active');
-          $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-          $('#mobile-body-overly').fadeOut();
-        }
-        return false;
-      }
-    }
-  });
-
-
+          else{
+            $('.contact-error').removeClass('hide').html('There is an error occured your request. Please try again.');
+          }
+      });
+  }
+  else{
+    $('.contact-error').removeClass('hide').html('Please fill the form correctly!');
+  }
 });
